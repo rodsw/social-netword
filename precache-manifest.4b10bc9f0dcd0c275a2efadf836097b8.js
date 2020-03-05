e2882172667afd08db50b9b14d1ccdb872afca58
Rod <rod@rodswift.com>
2020-03-06 08:37:27 +1100

    Updates

---
+++ precache-manifest.4b10bc9f0dcd0c275a2efadf836097b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d13331de41c045622339",
+    "url": "/social-netword/static/css/main.522bbc8f.chunk.css"
+  },
+  {
+    "revision": "d13331de41c045622339",
+    "url": "/social-netword/static/js/main.d13331de.chunk.js"
+  },
+  {
+    "revision": "28ce2e8d17af55920db8",
+    "url": "/social-netword/static/css/1.bee05e6e.chunk.css"
+  },
+  {
+    "revision": "28ce2e8d17af55920db8",
+    "url": "/social-netword/static/js/1.28ce2e8d.chunk.js"
+  },
+  {
+    "revision": "83258fbcb1bb8c96c4a1",
+    "url": "/social-netword/static/js/runtime~main.83258fbc.js"
+  },
+  {
+    "revision": "6c06f3569eae717cdbe158ff0e65c031",
+    "url": "/social-netword/index.html"
+  }
+];
